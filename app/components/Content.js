9d744b7a5f77de9d75ae55575582ea8017147239
Ali Akbar Khan <aliakbarkhan161@gmail.com>
2024-12-30 12:56:57 +0500

    Update Content.js

--- app/components/Content.js
+++ app/components/Content.js
@@ -7,7 +7,7 @@ export const Content = () => {
     <div className="content w-[100%] min-h-screen flex flex-col sm:flex-row justify-center items-start pt-5 pb-5 space-x-5 pl-16 pr-16 mt-32 ">
       <Profile />
       <div className=" dark:bg-gray-800 w-[82%] bg-white dark:text-white text-gray-700 space-y-4 rounded-2xl  flex flex-col items-center justify-between pb-3 pt-3 flex-1">
-        hii
+        main Dynamic Content Area
       </div>
 
       <div className=" dark:bg-gray-800 w-[5%] bg-white dark:text-white text-gray-700 space-y-4 rounded-2xl  flex flex-col items-center justify-between pb-8 pt-8">
